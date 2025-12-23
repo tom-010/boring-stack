ef87da6c4b2e71ca009d07533379ac2fec5dc1c0
Thomas Stadelmann <tom-010@web.de>
2025-12-23 23:04:53 +0100

    toward python

--- services/image2/main.py
+++ services/image2/main.py
@@ -1,37 +1,44 @@
-from concurrent import futures
+import asyncio
 from io import BytesIO
 
-import grpc
+from grpclib.server import Server, Stream
+from grpclib.utils import graceful_exit
 from PIL import Image
 
-import image_pb2
-import image_pb2_grpc
+from proto_out.image import ResizeRequest, ResizeResponse
 
 
-class ImageServicer(image_pb2_grpc.ImageServiceServicer):
-    def Resize(self, request, context):
+class ImageService:
+    async def Resize(self, stream: Stream[ResizeRequest, ResizeResponse]) -> None:
+        request = await stream.recv_message()
+        assert request is not None
+
         image = Image.open(BytesIO(request.image_data))
-
         resized = image.resize((request.width, request.height), Image.Resampling.LANCZOS)
 
         output = BytesIO()
         format = image.format or "PNG"
         resized.save(output, format=format)
 
-        return image_pb2.ResizeResponse(
+        response = ResizeResponse(
             image_data=output.getvalue(),
             format=format.lower(),
         )
+        await stream.send_message(response)
+
+    def __mapping__(self):
+        return {
+            "/image.ImageService/Resize": self.Resize,
+        }
 
 
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    image_pb2_grpc.add_ImageServiceServicer_to_server(ImageServicer(), server)
-    server.add_insecure_port("[::]:50051")
-    server.start()
-    print("gRPC server running on port 50051")
-    server.wait_for_termination()
+async def main():
+    server = Server([ImageService()])
+    with graceful_exit([server]):
+        await server.start("0.0.0.0", 50051)
+        print("gRPC server running on port 50051")
+        await server.wait_closed()
 
 
 if __name__ == "__main__":
-    serve()
+    asyncio.run(main())
